726ff7b85a2a3456bf654543c83717c9ec50c3f5
Moritz Kai Bennett Bischof <m.bischof@aloberlin.de>
2021-11-15 15:18:52 +0100

    improved sudoku_solver almost works perfectly, just the ending is kind of janky 3 numbers are left and i don't know why it loops theer back and forth.Very happy though with my progress :)

--- Programmierung/sudoku_solver.py
+++ Programmierung/sudoku_solver.py
@@ -13,62 +13,204 @@ grid = [[8, 0, 0, 0, 0, 7, 0, 0, 2],
 
 nos = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
-
+def close():
+    print("Error: Stopping execution\ngrid:")
+    pprint(grid)
+    quit()
+def update_stack():
+    update_rows()
+    update_columns()
+    update_quadrants()
+    update_cells()  #important that cells is last
+def valid_stack():
+    check_if_rows_valid()
+    check_if_columns_valid()
+    check_if_quadrants_valid()
+    check_if_cells_valid()
+def reset_stack():
+    reset_rows()
+    reset_columns()
+    reset_quads()
+    reset_cells()
+def print_stack():
+    print_rows()
+    print_columns()
+    print_quads()
+    print_cells()
 def reset_quads():
     global quadrants
     quadrants = [[] for x in range(9)]
 
-
 def print_quads():
-    for q in quadrants:
-        pprint(q)
-
+    print("quadrants:")
+    for q_no in range(9):
+        print("[{}]:".format(q_no),end="")
+        for n in nos:
+            if n in quadrants[q_no]:
+                print(n,end=",",flush=True)
+        print()
 
 def check_if_quadrants_valid():
     for q_no, q in enumerate(quadrants):
         have = []
         for e in q:
             if e in have:
-                print("double number in quadrant:", q_no)
+                print("duplicate number in quadrant:", q_no)
                 print_quads()
-                quit()
+                close()
             have.append(e)
 
+def get_quadrant(r,c):
+    quadrant = int(r/3)*3 + int(c/3)
 
-def reset_rows():
-    global rows
-    rows = [[] for x in range(9)]
+    return quadrant
 
-
-def print_rows():
-    global rows
-    for r in rows:
-        # left off here in implementing what i did for quadrants for rows and columns as well
-        pprint(r)
-
-
-check_if_rows_valid():
-    for q_no, q in enumerate(rows):
-        have = []
-        for e in q:
-            if e in have:
-                print("double number in quadrant:", q_no)
-                print_quads()
-                quit()
-            have.append(e)
-
-
-def update_quadrants():
+def update_quadrants():         #quadrant 0-8: top left to top right and then one row down
     global quadrants
     reset_quads()
     for q_no in range(9):
         for r in range(3*int(q_no/3), 3*(int(q_no/3)+1)):
             for c in range(3*(q_no % 3), (3*(q_no % 3+1))):
-                # print("row:",r,"\tcolumn:",c)
                 if grid[r][c] != 0:
                     quadrants[q_no].append(grid[r][c])
+    check_if_quadrants_valid()
 
+def reset_rows():
+    global rows
+    rows = [[] for x in range(9)]
 
-update_quadrants()
-print_quads()
-check_if_quadrants_valid()
+def print_rows():
+    print("rows:")
+    global rows
+    for r_no in range(9):
+        print("[{}]:".format(r_no),end="")
+        for n in nos:
+            if n in rows[r_no]:
+                print(n,end=",",flush=True)
+        print()
+
+def check_if_rows_valid():
+    for r_no, r in enumerate(rows):
+        have = []
+        for e in r:
+            if e in have:
+                print("duplicate number in row:", r_no)
+                print_rows()
+                close()
+            have.append(e)
+
+def update_rows():
+    global rows
+    reset_rows()
+    for r_no in range(9):
+        for c in range(9):
+            if grid[r_no][c] != 0:
+                rows[r_no].append(grid[r_no][c])
+    check_if_rows_valid()
+
+def reset_columns():
+    global columns
+    columns = [[] for x in range(9)]
+
+def print_columns():
+    print("columns:")
+    global columns
+    for c_no in range(9):
+        print("[{}]:".format(c_no),end="")
+        for n in nos:   
+            if n in columns[c_no]:
+                print(n,end=",",flush=True)
+        print()
+def check_if_columns_valid():
+    for c_no, c in enumerate(columns):
+        have = []
+        for e in c:
+            if e in have:
+                print("duplicate number in column:", c_no)
+                print_columns()
+                close()
+            have.append(e)
+
+def update_columns():
+    global columns
+    reset_columns()
+    for c in range(9):
+        for r in range(9):
+            if grid[r][c] != 0:
+                columns[c].append(grid[r][c])
+    check_if_columns_valid()
+
+def reset_cells():
+    global cells
+    cells = [[[]for y in range(9)] for x in range(9)]
+
+def print_cells():
+    global cells
+    print("cells:")
+    pprint(cells)
+
+def check_if_cells_valid():
+    for r in range(9):
+        for c in range(9):
+            have = []
+            for e in cells[r][c]:
+                if e in have:
+                    print("duplicate number in cells(r,c):", (r,c))
+                    print_cells()
+                    close()
+                have.append(e)
+def update_cells(): #3 dim array of possible numbers per cell
+    global cells
+    reset_cells()
+    for r in range(9):
+        for c in range(9):
+            for n in nos:
+                if n not in rows[r]:
+                    if n not in columns[c]:
+                        if n not in quadrants[get_quadrant(r,c)]:
+                            cells[r][c].append(n)
+    check_if_cells_valid()
+
+def solve_for_one_solution():
+    print("new attempt")
+    progress = None
+    global grid
+    for r in range(9):
+        for c in range(9):
+            if len(cells[r][c]) == 1:
+                grid[r][c] = cells[r][c][0]
+                print("solved: row={} column={} number={}".format(r,c,grid[r][c]))
+                progress = True
+                update_stack()
+    
+    return progress
+
+def check_if_finished():
+    nozero = True
+    for r in range(9):
+        for c in range(9):
+            if grid[r][c] == 0:
+                nozero = False
+    valid_stack()
+    return nozero
+    
+def loop():
+    reset_stack()
+    input("ready:")
+    for i in range(20):
+        pprint(grid)
+        print_stack()
+        update_stack()
+        check_if_finished()
+        if not solve_for_one_solution():
+            pprint(grid)
+            break
+# update_quadrants()
+# print_quads()
+# update_rows()
+# print_rows()
+# update_columns()
+# print_columns()
+# update_cells()
+# print_cells()
+loop()
